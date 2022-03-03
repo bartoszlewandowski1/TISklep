d62f234a7a783084fa624e2060299c5e1d6cf1e4
bartoszlewandowski1 <bartosz.lewandowski@student.ukw.edu.pl>
2022-03-03 15:47:46 +0100

    zmiana 2

---
+++ TISklep/Models/Film.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TISklep.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nie podano Tytułu")]
+        public string Tytul { get; set; }
+
+        public string Rezyser { get; set; }
+        [StringLength(1000)]
+        public string Opis { get; set; }
+
+        public decimal Cena { get; set; }
+        //znak zapytania po typie = nieobowiązkowa
+        public DateTime? DataProdukcji { get; set; }
+        //klucz obcy
+        public int KategoriaId { get; set; }
+
+        public Kategoria Kategoria { get; set; }
+
+    }
+}
---
+++ TISklep/Models/Kategoria.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TISklep.Models
+{
+    public class Kategoria
+    {
+        public int Id { get; set; }
+
+        public string Nazwa { get; set; }
+
+        public string Opis { get; set; }
+
+        public ICollection<Film> Filmy { get; set; }
+    }
+}
